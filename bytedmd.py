6dd7f6630553323430e338abd60bfe612581fea5
Yaroslav Bulatov <yaroslavvb@gmail.com>
2026-04-03 18:35:31 -0700

    simplify

--- bytedmd.py
+++ bytedmd.py
@@ -5,12 +5,11 @@ bytedmd(add, (1, 2)) calls add(1, 2) and returns ByteDMD_cost
 """
 
 import math
-import sys
 
 
 def usqrt(x):
     """Ceiling of square root."""
-    return math.isqrt(x - 1) + 1
+    return math.isqrt(x - 1) + 1 if x > 0 else 0
 
 
 class _TrackedContext:
@@ -18,33 +17,26 @@ class _TrackedContext:
 
     def __init__(self):
         self.stack = []     # LRU stack (rightmost = MRU)
-        self.trace = []  # list of access distances
+        self.trace = []     # list of access distances
         self._counter = 0
 
     def allocate(self):
         """Allocate a new entry at the top of the stack and return its key."""
         self._counter += 1
-        key = self._counter
-        self.stack.append(key)
-        return key
+        self.stack.append(self._counter)
+        return self._counter
 
     def read_all_then_move(self, keys):
-        """Read all operands at current distances, then move all to top."""
-        for key in keys:
-            if key is None:
-                continue
-            depth = 0
-            for k in reversed(self.stack):
-                if k == key:
-                    self.trace.append(depth + 1)
-                    break
-                depth += 1
-
+        """Read all operands at current distances, then sequentially move all to top."""
+        valid_keys = [k for k in keys if k is not None]
+        
+        # Calculate element depth relative to top (MRU)
+        self.trace.extend(len(self.stack) - self.stack.index(k) for k in valid_keys)
+        
         # Update LRU stack
-        for key in keys:
-            if key is not None:
-                self.stack.remove(key)
-                self.stack.append(key)
+        for k in valid_keys:
+            self.stack.remove(k)
+            self.stack.append(k)
 
 
 class _TrackedValue:
@@ -55,66 +47,39 @@ class _TrackedValue:
         self._key = key
         self.value = value
 
-    def _do_op(self, other, op_name, reverse=False):
-        if isinstance(other, _TrackedValue):
-            other_key, other_val = other._key, other.value
-        else:
-            other_key, other_val = None, other
 
-        keys = [other_key, self._key] if reverse else [self._key, other_key]
-        self._ctx.read_all_then_move(keys)
-
-        val1, val2 = (other_val, self.value) if reverse else (self.value, other_val)
-        res_val = getattr(val1, op_name)(val2)
-
-        if res_val is NotImplemented:
-            return NotImplemented
-        if isinstance(res_val, tuple):
-            return tuple(_TrackedValue(self._ctx, self._ctx.allocate(), x) for x in res_val)
-        return _TrackedValue(self._ctx, self._ctx.allocate(), res_val)
-
-    def _do_unary(self, op_name):
-        self._ctx.read_all_then_move([self._key])
-        res_val = getattr(self.value, op_name)()
-        return _TrackedValue(self._ctx, self._ctx.allocate(), res_val)
-
-    def _do_cmp(self, other, op_name):
-        if isinstance(other, _TrackedValue):
-            other_key, other_val = other._key, other.value
-        else:
-            other_key, other_val = None, other
-
-        keys = [self._key, other_key]
-        self._ctx.read_all_then_move(keys)
-
-        return getattr(self.value, op_name)(other_val)
+def _make_method(op, is_cmp=False, is_rev=False):
+    """Generic factory evaluating bin/unary/cmp operations with read penalties."""
+    def method(self, *args):
+        if not args:  # Unary
+            self._ctx.read_all_then_move([self._key])
+            res = getattr(self.value, op)()
+        else:  # Binary or Comparison
+            other = args[0]
+            okey, oval = (other._key, other.value) if isinstance(other, _TrackedValue) else (None, other)
+            self._ctx.read_all_then_move([okey, self._key] if is_rev else [self._key, okey])
+            
+            # Delegate reverse handling naturally to wrapped primitive (`int.__rsub__`, etc.)
+            res = getattr(self.value, op)(oval)
+            
+        if is_cmp or res is NotImplemented:
+            return res
+        if isinstance(res, tuple):
+            return tuple(_TrackedValue(self._ctx, self._ctx.allocate(), x) for x in res)
+        return _TrackedValue(self._ctx, self._ctx.allocate(), res)
+    return method
 
 
-# Binary arithmetic & bitwise operations
-_binops = 'add sub mul truediv floordiv mod divmod pow lshift rshift and xor or matmul'
-for _op in _binops.split():
-    def _make_fwd(name):
-        def method(self, o): return self._do_op(o, name)
-        return method
-    def _make_rev(name):
-        def method(self, o): return self._do_op(o, name, reverse=True)
-        return method
-    setattr(_TrackedValue, f'__{_op}__', _make_fwd(f'__{_op}__'))
-    setattr(_TrackedValue, f'__r{_op}__', _make_rev(f'__r{_op}__'))
+# Wire up Python magic methods dynamically
+for _op in 'add sub mul truediv floordiv mod divmod pow lshift rshift and xor or matmul'.split():
+    setattr(_TrackedValue, f'__{_op}__', _make_method(f'__{_op}__'))
+    setattr(_TrackedValue, f'__r{_op}__', _make_method(f'__r{_op}__', is_rev=True))
 
-# Unary operations
 for _op in 'neg pos abs invert'.split():
-    def _make_unary(name):
-        def method(self): return self._do_unary(name)
-        return method
-    setattr(_TrackedValue, f'__{_op}__', _make_unary(f'__{_op}__'))
+    setattr(_TrackedValue, f'__{_op}__', _make_method(f'__{_op}__'))
 
-# Comparisons (returned unwrapped for correct control flow)
 for _op in 'eq ne lt le gt ge'.split():
-    def _make_cmp(name):
-        def method(self, o): return self._do_cmp(o, name)
-        return method
-    setattr(_TrackedValue, f'__{_op}__', _make_cmp(f'__{_op}__'))
+    setattr(_TrackedValue, f'__{_op}__', _make_method(f'__{_op}__', is_cmp=True))
 
 
 def _wrap(ctx, val):
@@ -136,16 +101,12 @@ def _unwrap(val):
 
 
 def traced_eval(func, args):
-    """Run func with traced arguments.
-    Returns (trace, result) where trace is a list of distances.
-    """
+    """Run func with traced arguments. Returns (trace, result)."""
     ctx = _TrackedContext()
-    traced_args = [_wrap(ctx, val) for val in args]
-
-    ret = func(*traced_args)
-    return ctx.trace, _unwrap(ret)
+    return ctx.trace, _unwrap(func(*[_wrap(ctx, val) for val in args]))
 
 
 def bytedmd(func, args, bytes_per_element=1):
-    trace, result = traced_eval(func, args)
+    """Evaluate ByteDMD cost of running func with args."""
+    trace, _ = traced_eval(func, args)
     return sum(usqrt(int(d * bytes_per_element)) for d in trace)
